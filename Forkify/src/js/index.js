2343ba082935cbbea9a4fa468691c9a4fc39980e
Victor <victorlundhede@gmail.com>
2019-09-11 23:38:14 +0200

    Add babel to code

--- Forkify/src/js/index.js
+++ Forkify/src/js/index.js
@@ -1,4 +1,4 @@
 // Global app controller
 import x from './test';
-
-console.log(`I imported ${x} from another module!`);
+const y = 23;
+console.log(`I imported ${x} from another module! Variable y is ${y}`);
